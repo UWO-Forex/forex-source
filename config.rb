7c9295dfacb102eb2c5b0f56cc8b1a902f16c9fd
Scott Mackie <scott@mackie.io>
2016-09-12 15:47:44 -0400

    updating build process

--- config.rb
+++ config.rb
@@ -1,8 +1,5 @@
 activate :automatic_image_sizes
-
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+activate :directory_indexes
 
 activate :deploy do |deploy|
   deploy.method = :git
