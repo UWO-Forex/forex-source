6c9f5c4c161c028ad4ffea5f6f461c7fef4fceb9
Scott Mackie <scott@mackie.io>
2016-09-12 15:31:08 -0400

    testing this

--- config.rb
+++ config.rb
@@ -3,10 +3,9 @@ activate :automatic_image_sizes
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-set :build_dir, '../'
 
 activate :deploy do |deploy|
-  deploy.deploy_method = :git
+  deploy.method = :git
   deploy.remote   = 'https://github.com/UWO-Forex/UWO-Forex.github.io.git'
   deploy.branch   = 'master'
 end
